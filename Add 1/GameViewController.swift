32996f43e00493cc88a6b7f55d3e407248136191
Francisco Pedraza <pacopedraza87@gmail.com>
2019-12-01 14:04:16 -0800

    Adds random function and String extend function
    Added:
    1. Coding The “viewDidLoad()” Function
    2. Updating The Score And Number Labels
    3. Generating Random Number Strings
    4. Outlets: Connecting Code With UI
    
    Signed-off-by: Francisco Pedraza <pacopedraza87@gmail.com>

--- Add 1/GameViewController.swift
+++ Add 1/GameViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    @IBOutlet weak var scoreLabel:UILabel?
+    @IBOutlet weak var timeLabel:UILabel?
+    @IBOutlet weak var numberLabel:UILabel?
+    @IBOutlet weak var inputField:UITextField?
+
+    var score = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateScoreLabel()
+        updateNumberLabel()
     }
 
+    func updateScoreLabel() {
+        scoreLabel?.text = String(score)
+    }
+
+    func updateNumberLabel() {
+        numberLabel?.text = String.randomNumber(length: 4)
+    }
 
 }
 
---
+++ Add 1/String.swift
@@ -0,0 +1,22 @@
+//
+//  String.swift
+//  Add 1
+//
+//  Created by pacopedraza on 12/1/19.
+//  Copyright © 2019 pacopedraza. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    static func randomNumber(length: Int) -> String
+    {
+        var result = ""
+
+        for _ in 0..<length {
+            let digit = Int.random(in: 0...9)
+            result += "\(digit)" // string interpolation, convert the integer to string.
+        }
+        return result
+    }
+}
